dcb6f5a45fe6836a8ec9ce7d77defeb4065808cb
Quargle <ph31pe@gmail.com>
2021-12-14 14:52:18 +0000

    new deploy

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,17 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/quargle/portfolio.git', // Update to point to your repository  
-        user: {
-            name: 'Peter Evans', // update to use your name
-            email: 'ph31pe@gmail.com' // Update to use your email
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/el3um4s/petits-chevaux.git', // Update to point to your repository
+		user: {
+			name: 'Samuele', // update to use your name
+			email: 'samuele@stranianelli.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
