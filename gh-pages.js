9797f74ce1126327b1d8df9ec27a9a4d355ae67e
Quargle <ph31pe@gmail.com>
2021-12-14 14:57:04 +0000

    more fixes for gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/Quargle/Portfolio.git', // Update to point to your repository
+		repo: 'https://github.com/quargle/portfolio.git', // Update to point to your repository
 		user: {
 			name: 'Peter Evans', // update to use your name
 			email: 'ph31pe@gmail.com' // Update to use your email
