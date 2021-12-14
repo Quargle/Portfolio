33c38cf89190df6c7996bfa92ead543810378c51
Quargle <ph31pe@gmail.com>
2021-12-14 14:52:52 +0000

    fix error in gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/el3um4s/petits-chevaux.git', // Update to point to your repository
+		repo: 'https://github.com/Quargle/Portfolio.git', // Update to point to your repository
 		user: {
 			name: 'Samuele', // update to use your name
 			email: 'samuele@stranianelli.com' // Update to use your email
